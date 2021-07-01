019df9b20224dad8ff7c5149109e9fbbda8a50b7
Antoine Kassi <61750131+antonio2505@users.noreply.github.com>
2021-07-02 00:33:58 +0530

    Thompson Sampling Model with R

---
+++ Thompson_Sampling.R
@@ -0,0 +1,40 @@
+# THOMPSON SAMPLING MODEL
+
+## Importing the dataset
+dataset = read.csv('Ads_CTR_Optimisation.csv')
+
+# Implementing THOMPSON SAMPLING
+N = 10000
+d = 10
+ads_selected = integer(0)
+number_of_reward_1 = integer(d)
+number_of_reward_0 = integer(d)
+total_reward = 0
+for (n in 1:N) {
+  ad = 0
+  max_random = 0
+  for (i in 1:d) {
+    random_beta = rbeta(n = 1, 
+                        shape1 = number_of_reward_1[i] + 1,
+                        shape2 = number_of_reward_0[i] + 1)
+    if (random_beta > max_random) {
+      max_random = random_beta
+      ad = i
+    }
+  }
+  ads_selected = append(ads_selected, ad)
+  reward = dataset[n, ad]
+  if (reward == 1){
+    number_of_reward_1[ad] = number_of_reward_1[ad] + 1
+  }else {
+    number_of_reward_0[ad] = number_of_reward_0[ad] + 1
+  }
+  total_reward = total_reward + reward
+}
+
+# Visualising the results
+hist(ads_selected,
+     col = 'blue',
+     main = 'Histogram of ads selections',
+     xlab = 'Ads',
+     ylab = 'Number of times each ad was selected')
